3dfb189b8205daf14ea54a3451318c9721075e61
Qixun Qu <quqixun@gmail.com>
2018-04-14 20:56:20 +0200

    Add more commands to train model using different volumes

--- src/auto_run.sh
+++ src/auto_run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 python add.py --paras=paras-1 --volume=whole
-python add.py --paras=paras-2 --volume=whole
+python add.py --paras=paras-1 --volume=gm
+python add.py --paras=paras-1 --volume=wm
+python add.py --paras=paras-1 --volume=csf
+
+python add.py --paras=paras-2 --volume=gm
+python add.py --paras=paras-2 --volume=wm
+python add.py --paras=paras-2 --volume=csf
