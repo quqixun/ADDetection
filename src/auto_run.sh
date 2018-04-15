8df3bc4b08aa97206ca09e601952ce0d5d8f465c
Qixun Qu <quqixun@gmail.com>
2018-04-15 15:27:44 +0200

    Add more commands and comments

--- src/auto_run.sh
+++ src/auto_run.sh
@@ -1,10 +1,44 @@
 #!/bin/sh
 
-python add.py --paras=paras-1 --volume=whole
-python add.py --paras=paras-1 --volume=gm
-python add.py --paras=paras-1 --volume=wm
-python add.py --paras=paras-1 --volume=csf
+#
+# Section 1
+#
+# Train and test model
+# Command:
+# python add.py --paras=paras_name --volume=volume_type
+# Parameters:
+# - paras: hyperparameters set in hyper_paras.json
+# - volume: one type volume of "whole", "gm", "wm" or "csf"
+
+# python add.py --paras=paras-1 --volume=whole
+# python add.py --paras=paras-1 --volume=gm
+# python add.py --paras=paras-1 --volume=wm
+# python add.py --paras=paras-1 --volume=csf
 
 python add.py --paras=paras-2 --volume=gm
 python add.py --paras=paras-2 --volume=wm
 python add.py --paras=paras-2 --volume=csf
+
+
+# To train the model using non-separated samples,
+# in pre_paras.json:
+# - change "data_dir" to the directory which
+#   contains non-separated samples
+#   NOTE: not the absolute path, but the relative
+#         path within project directory
+# - change "subj_separated" to true
+
+# python add.py --paras=paras-3 --volume=whole
+
+
+#
+# Section 2
+#
+# Train and test model respectively
+# Commands:
+# python add_train.py --paras=paras_name --volume=volume_type
+# python add_test.py --paras=paras_name --volume=volume_type
+# Same parameters as in Section 1
+
+# python add_train.py --paras=paras-1 --volume=whole
+# python add_test.py --paras=paras-1 --volume=whole
